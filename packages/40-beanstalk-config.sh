a45827ca611842f2fe2eaab24351c9cd967a8844
haruna <w10776e8w@yahoo.co.jp>
2025-05-12 08:22:43 +0900

    chore: heredoc

--- packages/40-beanstalk-config.sh
+++ packages/40-beanstalk-config.sh
@@ -2,6 +2,7 @@
 set -eux
 
 echo "Setting beanstalkd ..."
-echo 'BEANSTALKD_LISTEN_ADDR=127.0.0.1' >/tmp/beanstalkd
-echo 'BEANSTALKD_LISTEN_PORT=4229' >>/tmp/beanstalkd
-mv /tmp/beanstalkd /etc/default/beanstalkd
+cat <<'EOF' >/etc/default/beanstalkd
+BEANSTALKD_LISTEN_ADDR=127.0.0.1
+BEANSTALKD_LISTEN_PORT=4229
+EOF

0c85410e66292fcb38191330dcfda1dc358e2133
eggplants <w10776e8w@yahoo.co.jp>
2025-05-18 22:08:45 +0900

    chore: exclude meaningless patches

--- packages/41-asterisk.sh
+++ packages/41-asterisk.sh
@@ -4,23 +4,11 @@ set -eux
 # https://github.com/asterisk/asterisk/releases
 LIB_VERSION='16.30.1'
 LIB_URL="https://github.com/asterisk/asterisk/releases/download/${LIB_VERSION}/asterisk-${LIB_VERSION}.tar.gz"
-LIB_NAME='asterisk'
 srcDirName=$(downloadFile "$LIB_URL")
+
 pushd "$srcDirName"
 
-ROOT_DIR="$(realpath "$(dirname "$0")")"
-PATCH_PATH="$ROOT_DIR/patches/${LIB_NAME}"
-if [ -d "$PATCH_PATH" ]; then
-  for filename in "$PATCH_PATH"/*.patch; do
-    [ -e "$filename" ] || continue
-    echo "Starting $filename"
-    (
-      patch -p1 -i "$filename"
-    )
-  done
-fi
 contrib/scripts/get_mp3_source.sh
-# contrib/scripts/install_prereq install
 ./configure
 make menuselect.makeopts
 case "$(dpkg --print-architecture)" in
@@ -66,5 +54,6 @@ chown -R asterisk:asterisk /storage/usbdisk1/mikopbx/persistence \
   /var/lib/asterisk \
   /var/spool/asterisk \
   /var/log/asterisk
+
 popd
 rm -rf "$srcDirName" ./zephir
